5d1dc45df0e93059c119dcef21b2057ccd45035c
Greg Fruin <gfruin@gmail.com>
2019-05-12 17:19:21 -0500

    made connections in db.js

---
+++ db/db.js
@@ -0,0 +1,23 @@
+//require mongoose
+const mongoose = require('mongoose');
+//make connection string?
+// const connectionString = 'mongodb://localhost/'
+//connect mongoose
+mongoose.connect('mongodb://localhost/news', {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+})
+//connection.on connect
+mongoose.connection.on('connected', () => {
+	console.log('Mongoose is connected');
+})
+//connection.on disconnect
+mongoose.connection.on('disconnected', () => {
+	console.log('Mongoose is disconnected');
+})
+
+//connection.on error
+mongoose.connnection.on('error', (error) => {
+	console.log('mongoose hit an error', error);
+})
