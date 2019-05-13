88b2b727ba93af425cc2cb9f744419d17386f337
Greg Fruin <gfruin@gmail.com>
2019-05-13 08:15:55 -0500

    added app listener 9000 in server.js

--- server.js
+++ server.js
@@ -6,8 +6,44 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 const session = require('express-session');
 
+require('./db/db');
+
 //middleware here
+app.use(session({
+	secret: 'fake news',
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// const corsOptions = {
+// 	origin: 'http://localhost:3000',
+// 	credentials: true,
+// 	optionsSuccessStatus: 200
+// }
+
+// app.use(cors(corsOptions));
 
 //controllers here
 
+const articleController = require('./controllers/articleController');
+const authController = require('./controllers/authController');
+const commentController = require('./controllers/commentController');
+const userController = require('./controllers/userController');
+
+app.use('/api/v1/articles', articleController)
+// app.use('/auth', authController)
+
 // app listener here
+
+app.listen(process.env.PORT || 9000, () => {
+	console.log('listening on PORT 9000');
+})
+
+
+
+
+
+
