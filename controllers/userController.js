d3543ae1abe469e2b5262ddd4355bd70c9578bee
Greg Fruin <gfruin@gmail.com>
2019-05-13 10:27:50 -0500

    added index route and show route for userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
+
+router.get('/', async (req,res,next) => {
+	console.log(req.body, 'this is all the users');
+	try {
+		const allUsers = await User.find();
+		res.json({
+			status: 200,
+			data: allUsers
+		})
+	} catch(err) {
+		next(err)
+	}
+})
+
+
+
 router.post('/', async (req, res, next) => {
 	console.log(req.body, 'this is the session')
 	try {
@@ -10,16 +26,30 @@ router.post('/', async (req, res, next) => {
 		req.session.username = req.body.username;
 		res.json({
 			status: 200,
-			data: 'login was successful'
+			data: createdUser
 		})
 	} catch(err) {
 		console.log(err);
-		res.send(err)
+		next(err)
 	}
 
 
 })
 
+router.get('/:id', async (req,res,next) => {
+	try {
+		const foundUser = await User.findById(req.params.id)
+		res.json({
+			status: 200,
+			data: foundUser
+		})
+	} catch(err) {
+		console.log(err);
+		next(err)
+	}
+})
+
+router.delete('/:id')
 
 
 
