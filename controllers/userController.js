e5c27be676b2cd6d53831664b77aa8320fb82631
Greg Fruin <gfruin@gmail.com>
2019-05-13 10:42:05 -0500

    added edit and update routes to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 
-
+//user index route
 router.get('/', async (req,res,next) => {
 	console.log(req.body, 'this is all the users');
 	try {
@@ -14,10 +14,10 @@ router.get('/', async (req,res,next) => {
 	} catch(err) {
 		next(err)
 	}
-})
+}) //end of user index route
 
 
-
+//user create route
 router.post('/', async (req, res, next) => {
 	console.log(req.body, 'this is the session')
 	try {
@@ -34,8 +34,9 @@ router.post('/', async (req, res, next) => {
 	}
 
 
-})
+}) //end of user create route
 
+//user show route
 router.get('/:id', async (req,res,next) => {
 	try {
 		const foundUser = await User.findById(req.params.id)
@@ -47,9 +48,54 @@ router.get('/:id', async (req,res,next) => {
 		console.log(err);
 		next(err)
 	}
+}) //end of user show route
+
+
+//user delete route
+router.delete('/:id', async (req,res,next) => {
+	try {
+		const deletedUser = await User.findByIdAndRemove(req.params.id);
+		res.json({
+			status: 200,
+			data: deletedUser
+		})
+
+	} catch(err) {
+		next(err)
+	}
+}) //end of user delete route
+
+//user edit route
+
+router.get('/:id/edit', async (req,res,next) => {
+	try {
+		const foundUser = await User.findById(req.params.id)
+		res.json({
+			status: 200,
+			data: foundUser
+		})
+
+	} catch(err) {
+		next(err)
+	}
+
+}) //end of user edit route
+
+//user update route
+
+router.put('/:id', async (req,res,next) => {
+	try {
+		const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new:true})
+		res.json({
+			status: 200,
+			data: updatedUser
+		})
+	} catch(err) {
+		next(err)
+	}
 })
 
-router.delete('/:id')
+
 
 
 
