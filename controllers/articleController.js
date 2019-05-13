e0167bc30af5055a7f43e99358c324b258bacb5f
Greg Fruin <gfruin@gmail.com>
2019-05-13 12:27:41 -0500

    delete route works in articleController

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -18,7 +18,7 @@ router.get('/', async (req,res,next) => {
 	} catch(err) {
 		next(err)
 	}
-})
+}) //end of article index route
 
 router.post('/', async (req,res,next) => {
 	console.log('this is the article create route');
@@ -40,6 +40,45 @@ router.post('/', async (req,res,next) => {
 	}
 
 
+}) //end of article creation route
+
+// article show route
+
+router.get('/:id', async (req,res,next) => {
+	try {
+		const foundArticle = await 
+		Article.findById(req.params.id)
+		// Article.populate({ 
+		// 	path: 'comments',
+		// 	populate: {
+		// 		path: 'user'
+		// 	}
+
+		// })
+		res.json({
+			status: 200,
+			data: foundArticle
+		})
+	} catch(err){
+		next(err)
+	}
+}) //end of article show route
+
+//article delete route
+router.delete('/:id', async (req,res,next) => {
+	//need to add req.session.userDBId == null when I set up the user
+	try {
+		//need to add foundUser by findById 
+		const deletedArticle = await Article.findByIdAndRemove(req.params.id)
+		//need to remove the article from the user's posts
+		//need to update the user's db info
+		res.json({
+			status: 200,
+			data: deletedArticle
+		})
+	} catch(err) {
+		next(err)
+	}
 })
 
 
