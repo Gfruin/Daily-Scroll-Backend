c7c5ecb27067e8daf983248152b71e33538b731f
Greg Fruin <gfruin@gmail.com>
2019-05-14 18:16:25 -0500

    added .env to .gitignore

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ app.use(session({
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-// const corsOptions = {
-// 	origin: 'http://localhost:3000',
-// 	credentials: true,
-// 	optionsSuccessStatus: 200
-// }
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true,
+	optionsSuccessStatus: 200
+}
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 //controllers here
 
